cfe1597f2e12177bdbc3415ec32ea0e6a64447f5
PNWHiker <53063554+pnwhiker@users.noreply.github.com>
2020-09-24 13:38:24 -0700

    Further edits to App

--- 1-adding-and-removing-names/2-registering-names/start/app.js
+++ 1-adding-and-removing-names/2-registering-names/start/app.js
@@ -2,17 +2,54 @@
 
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
+
+
+function createLi (text) {
+    const li = document.createElement('li');
+    li.textContent = text;
+    const label = document.createElement('label')
+    label.textContent = 'Confirmed';
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox';
+    label.appendChild(checkbox);
+    li.appendChild(label);    
+    const button = document.createElement('button')
+    button.textContent = 'Remove';
+    li.appendChild(button);
+    return li;
+
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     // console.log(input.value);
     const text = input.value;
-    const ul = document.getElementById('invitedList');
-    const li = document.createElement('li');
-    li.textContent = text;
-    ul.appendChild(li);
-    input.value = '';
+    input.value = '';   
     
+    const li = createLi(text);
+    ul.appendChild(li);
+})
 
+ul.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        const li = e.target.parentNode;
+        const ul = li.parentNode;
+        ul.removeChild(li);
+    }
 
 })
 
+ul.addEventListener('change', (e) => {
+    const checkbox = e.target;
+    const checked = checkbox.checked;
+    const listItem = checkbox.parentNode.parentNode;
+    
+    if (checked) {
+        listItem.className = 'responded'
+
+    } else {
+        listItem.className = '';
+
+    }
+});
